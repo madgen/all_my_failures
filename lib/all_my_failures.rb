a0bc4d5e8b2bedbef6123b44aa061f9865fae1c8
Mistral Contrastin <me@madgen.net>
2016-11-16 19:40:02 +0000

    Add header to estimate current progress

--- lib/all_my_failures.rb
+++ lib/all_my_failures.rb
@@ -6,8 +6,10 @@ require 'parallel'
 class AllMyFailures
   PROGRAM_NAME = 'all_my_failures'
   VERSION = '0.0.1'
+  TERMINAL_WIDTH_R = /^\d\d (?<cols>\d\d)$/
 
   def self.run(session, failure_output: nil, success_output: nil)
+    print header
     session.run
     puts
     puts session
@@ -23,5 +25,31 @@ class AllMyFailures
         f.puts session.failures
       end
     end
+  end
+
+  private_class_method
+  def self.header
+    # Attempt to retrieve column width using stty UNIX utility
+    n_of_cols = TERMINAL_WIDTH_R.match(`stty size`)&.[](:cols)&.to_i
+
+    # Only create header if we know how many columns there are
+    if n_of_cols
+      header = '|' * n_of_cols
+
+      (10...n_of_cols).step(10) do |i|
+        marker_beg = i - 1
+        marker = i.to_s
+        marker_end = marker_beg + marker.length
+        container = header[marker_beg...marker_end]
+
+        # Only add the marker if there are enough columns left
+        if container.length == marker.length
+          header[marker_beg...marker_end] = marker
+        end
+      end
+
+      header << "\n"
+    end
+    header
   end
 end
